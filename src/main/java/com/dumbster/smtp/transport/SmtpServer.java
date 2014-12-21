f1673b7fb35f0689836634215ce07a69390a2de5
Saksham Gautam <saksham.gautam@gmail.com>
2014-12-21 09:34:02 +0100

    Refactored Listener

--- src/main/java/com/dumbster/smtp/transport/SmtpServer.java
+++ src/main/java/com/dumbster/smtp/transport/SmtpServer.java
@@ -2,19 +2,25 @@ package com.dumbster.smtp.transport;
 
 import com.dumbster.smtp.exceptions.SmtpException;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 import org.apache.log4j.Logger;
 import org.springframework.util.Assert;
 
 import java.util.List;
+import java.util.Map;
 
 public class SmtpServer implements Runnable, Observable<SmtpMessage>, Observer<SmtpMessage> {
     private static final Logger logger = Logger.getLogger(SmtpServer.class);
+    private static final Object serverLock = new Object();
     private final int port;
     private boolean stopped;
     private List<Observer<SmtpMessage>> observers;
@@ -30,6 +36,34 @@ public class SmtpServer implements Runnable, Observable<SmtpMessage>, Observer<S
         this.workerGroup = new NioEventLoopGroup();
     }
 
+    public static SmtpServer start(int port) throws Exception {
+        SmtpServer smtpServer = new SmtpServer(port);
+        logger.info("Starting SMTP server on port: " + port + "...");
+
+        synchronized (serverLock) {
+            Thread smtpServerThread = new Thread(smtpServer);
+            smtpServerThread.start();
+            serverLock.wait();
+        }
+
+        Assert.isTrue(!smtpServer.isStopped());
+        return smtpServer;
+    }
+
+    public boolean isStopped() {
+        return this.stopped;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            start();
+        } catch (Exception ex) {
+            throw new SmtpException(ex);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         int port;
         if (args.length > 0) {
@@ -41,31 +75,6 @@ public class SmtpServer implements Runnable, Observable<SmtpMessage>, Observer<S
         System.in.read();
     }
 
-    public static SmtpServer start(int port) throws Exception {
-        SmtpServer smtpServer = new SmtpServer(port);
-        logger.info("Starting SMTP server on port: " + port + "...");
-        Thread smtpServerThread = new Thread(smtpServer);
-
-        synchronized (smtpServer) {
-            smtpServerThread.start();
-            smtpServer.wait();
-        }
-        Assert.isTrue(!smtpServer.isStopped());
-        return smtpServer;
-    }
-
-    public boolean isStopped() {
-        return this.stopped;
-    }
-
-    @Override
-    public void run() {
-        try {
-            start();
-        } catch (Exception ex) {
-            throw new SmtpException(ex);
-        }
-    }
 
     private void start() throws Exception {
         if (!stopped) {
@@ -82,21 +91,50 @@ public class SmtpServer implements Runnable, Observable<SmtpMessage>, Observer<S
             b.childOption(ChannelOption.SO_KEEPALIVE, true);
 
             ChannelFuture f = b.bind(port).sync();
-            f.addListener(new SmtpServerBoundListener(this));
+            f.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                    synchronized (serverLock) {
+                        logger.info("Started SMTP server!");
+                        serverLock.notifyAll();
+                    }
+                }
+            });
             f.channel().closeFuture().sync();
         } finally {
             stop();
         }
     }
 
-    public void stop() {
+    public void stop() throws Exception {
         if (stopped) {
             return;
         }
+        logger.info("Stopping SMTP server...");
         stopped = true;
-        workerGroup.shutdownGracefully();
-        bossGroup.shutdownGracefully();
+        Future<?> future = workerGroup.shutdownGracefully();
+        future.addListener(new GenericFutureListener() {
+            @Override
+            public void operationComplete(Future future) throws Exception {
+                bossGroup.shutdownGracefully().addListener(new GenericFutureListener() {
+                    @Override
+                    public void operationComplete(Future future) throws Exception {
+                        synchronized (serverLock) {
+                            logger.info("Stopped SMTP server");
+                            serverLock.notifyAll();
+                        }
+                    }
+                });
+            }
+
+        });
+        synchronized (serverLock) {
+            serverLock.wait();
+        }
     }
+
+
+
 
     @Override
     public void notify(SmtpMessage data) {

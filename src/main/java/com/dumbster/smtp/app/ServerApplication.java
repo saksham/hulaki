bb48dafc152a04bd9c7af1217fd93ef8902674cf
Saksham Gautam <saksham.gautam@partner.wirecard.com>
2014-12-22 19:38:28 +0100

    Renamed the server application because dumbster is not exactly a mock, it is a fake.
    http://martinfowler.com/articles/mocksArentStubs.html

--- src/main/java/com/dumbster/smtp/app/ServerApplication.java
+++ src/main/java/com/dumbster/smtp/app/ServerApplication.java
@@ -6,16 +6,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 
 
-public class MockServerApp implements ApplicationListener<ContextStartedEvent> {
+@Component
+public class ServerApplication implements ApplicationListener<ContextStartedEvent> {
 
     public static void main(String[] args) throws Exception {
-        ClassLoader classLoader = MockServerApp.class.getClassLoader();
+        ClassLoader classLoader = ServerApplication.class.getClassLoader();
         URL appConfigResource = classLoader.getResource("application-config.xml");
         assert appConfigResource != null;
 

cd4bdd8535c4691d463796b7ca587bd07c5702d0
Saksham Gautam <saksham.gautam@gmail.com>
2014-12-23 07:32:34 +0100

    Fixes #16

--- src/main/java/com/dumbster/smtp/api/ApiClient.java
+++ src/main/java/com/dumbster/smtp/api/ApiClient.java
@@ -19,6 +19,7 @@ import com.dumbster.smtp.exceptions.SmtpTimeoutException;
 import com.dumbster.smtp.utils.EmailUtils;
 import com.google.common.collect.Lists;
 import org.apache.log4j.Logger;
+import org.springframework.util.Assert;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -63,33 +64,44 @@ public class ApiClient implements IApiClient {
 
     @Override
     public int countMails() {
-        return countMails(null);
+        logger.debug("Counting ALL emails stored on the server...");
+        CountRequest countRequest = new CountRequest();
+        String response = sendRequestToApiServer(countRequest);
+        CountResponse cResponse = unmarshalResponse(response, CountResponse.class);
+        logger.debug("Counted " + cResponse.getCount() + " emails");
+        return cResponse.getCount();
     }
 
     @Override
     public int countMails(String recipient) {
+        Assert.notNull(recipient);
+        
+        logger.debug("Counting emails for recipient: " + recipient + "...");
         CountRequest countRequest = new CountRequest();
-        if (recipient != null) {
-            String normalizedEmail = EmailUtils.normalizeEmailAddress(recipient);
-            countRequest.setRecipient(normalizedEmail);
-        }
+        String normalizedEmail = EmailUtils.normalizeEmailAddress(recipient);
+        countRequest.setRecipient(normalizedEmail);
+        
         String response = sendRequestToApiServer(countRequest);
         CountResponse cResponse = unmarshalResponse(response, CountResponse.class);
+        logger.debug("Counted " + cResponse.getCount() + " emails sent to " + recipient);
         return cResponse.getCount();
     }
 
     @Override
     public void clearMails(String recipient) {
+        logger.debug("Clearing emails for recipient: " + recipient + "...");
         String normalizedEmail = EmailUtils.normalizeEmailAddress(recipient);
         ClearRequest clearRequest = new ClearRequest().setRecipient(normalizedEmail);
 
         String response = sendRequestToApiServer(clearRequest);
         StatusResponse sResponse = unmarshalResponse(response, StatusResponse.class);
         assert (sResponse.getStatus() == 200);
+        logger.debug("All emails cleared for recipient: " + recipient);
     }
 
     @Override
     public List<MailMessage> getMessages(String recipient) {
+        logger.debug("Retrieving emails for recipient: " + recipient + "...");
         String normalizedEmail = EmailUtils.normalizeEmailAddress(recipient);
         GetRequest getRequest = new GetRequest().setRecipient(normalizedEmail);
 
@@ -97,12 +109,13 @@ public class ApiClient implements IApiClient {
         response = sendRequestToApiServer(getRequest);
 
         GetResponse getResponse = unmarshalResponse(response, GetResponse.class);
+        logger.debug("Retrieved " + getResponse.getMessages().size() + " emails for recipient: " + recipient);
         return getResponse.getMessages();
     }
 
     @Override
     public List<MailMessage> getMessagesBySubject(String recipient, String subject) {
-        logger.debug("Getting emails matching subject " + subject + " for email address " + recipient);
+        logger.debug("Getting emails matching subject " + subject + " for email address " + recipient + "...");
         List<MailMessage> allMessages = getMessages(recipient);
         List<MailMessage> matchingMessages = Lists.newArrayList();
         for (MailMessage message : allMessages) {
@@ -111,52 +124,65 @@ public class ApiClient implements IApiClient {
                 matchingMessages.add(message);
             }
         }
+
+        logger.debug("Retrieved " + matchingMessages.size() + " emails for recipient: " + recipient);
         return matchingMessages;
     }
 
     @Override
     public boolean isRelayRecipient(String recipient) {
+        logger.debug("Checking if " + recipient + " is a relay recipient...");
         String normalizedEmail = EmailUtils.normalizeEmailAddress(recipient);
         RelayRequest relayRequest = new RelayRequest(RelayMode.GET);
         relayRequest.setRecipient(normalizedEmail);
         String response = sendRequestToApiServer(relayRequest);
         StatusResponse statusResponse = unmarshalResponse(response, StatusResponse.class);
-        return (statusResponse.getStatus() == 200);
+        boolean isRelayRecipient = statusResponse.getStatus() == 200;
+        logger.debug(recipient + ((isRelayRecipient) ? " is" : " isn't") + " a relay recipient");
+        return isRelayRecipient;
     }
 
     @Override
     public void addRelayRecipient(String recipient) {
+        logger.debug("Making " + recipient + " a relay recipient...");
         String normalizedEmail = EmailUtils.normalizeEmailAddress(recipient);
         RelayRequest relayRequest = new RelayRequest(RelayMode.ADD);
         relayRequest.setRecipient(normalizedEmail);
         String response = sendRequestToApiServer(relayRequest);
         StatusResponse statusResponse = unmarshalResponse(response, StatusResponse.class);
         assert (statusResponse.getStatus() == 200);
+        logger.debug(recipient + " is now a relay recipient.");
     }
 
     @Override
     public void removeRelayRecipient(String recipient) {
+        logger.debug("Removing " + recipient + " from list of relay recipients...");
         String normalizedEmail = EmailUtils.normalizeEmailAddress(recipient);
         RelayRequest relayRequest = new RelayRequest(RelayMode.REMOVE);
         relayRequest.setRecipient(normalizedEmail);
         String response = sendRequestToApiServer(relayRequest);
         StatusResponse statusResponse = unmarshalResponse(response, StatusResponse.class);
         assert (statusResponse.getStatus() == 200);
+        logger.debug(recipient + " is no more a relay recipient.");
     }
 
     @Override
     public void clearRelayRecipients() {
+        logger.debug("Removing ALL relay recipients...");
         RelayRequest relayRequest = new RelayRequest(RelayMode.REMOVE);
         String response = sendRequestToApiServer(relayRequest);
         StatusResponse statusResponse = unmarshalResponse(response, StatusResponse.class);
         assert (statusResponse.getStatus() == 200);
+        logger.debug("Relay recipient list cleared.");
     }
 
     @Override
     public List<String> getRelayRecipients() {
+        logger.debug("Retrieving the list of relay recipients...");
         RelayRequest relayRequest = new RelayRequest(RelayMode.GET);
         String response = sendRequestToApiServer(relayRequest);
         RelayResponse relayResponse = unmarshalResponse(response, RelayResponse.class);
+        logger.debug("Currently " + relayResponse.getRelayRecipients().getRecipients().size() + " relay recipieints are registered.");
         return relayResponse.getRelayRecipients().getRecipients();
     }
 
@@ -173,10 +199,14 @@ public class ApiClient implements IApiClient {
         try {
             Socket clientSocket = new Socket(this.apiServerHostname, this.apiServerPort);
             DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            out.writeBytes(request.toRequestString() + "\n");
+            String requestString = request.toRequestString() + "\n";
+            logger.trace(requestString);
+            out.writeBytes(requestString);
             StringBuilder response = new StringBuilder();
+            logger.trace(response.toString());
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine).append("\n");
